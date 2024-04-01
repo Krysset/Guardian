c24f95abb8e0d1722d245379c8f8b52a63e1b1d0
Krysset <maxhagman@hotmail.se>
2024-04-01 03:46:44 +0200

    Updated login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,11 +2,16 @@ export default function LoginPage() {
 	return (
 		<body>
 			<main className="container">
-				<form>
-					<input type="username" placeholder="Username" required />
-					<input type="password" placeholder="Password" required />
-					<button type="submit">Login</button>
-				</form>
+				<article>
+					<header>Login to your account</header>
+					<form>
+						<fieldset role="group">
+							<input type="username" placeholder="Username" required />
+							<input type="password" placeholder="Password" required />
+							<button type="submit">Login</button>
+						</fieldset>
+					</form>
+				</article>
 			</main>
 		</body>
 	);
